a0519053567d409177a2f0ab170c7e600a7de903
Kananel0 <158855994+Kananel0@users.noreply.github.com>
2026-03-20 22:58:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,9 @@ app.get('/', (req, res) => {
   return res.status(200).send({'message': 'SHIPTIVITY API. Read documentation to see API docs'});
 });
 
-// We are keeping one connection alive for the rest of the life application for simplicity
 const db = new Database('./clients.db');
 
-// Don't forget to close connection when server gets terminated
+
 const closeDb = () => db.close();
 process.on('SIGTERM', closeDb);
 process.on('SIGINT', closeDb);
@@ -67,69 +66,76 @@ const validatePriority = (priority) => {
 
 /**
  * Get all of the clients. Optional filter 'status'
- * GET /api/v1/clients?status={status} - list all clients, optional parameter status: 'backlog' | 'in-progress' | 'complete'
+ * GET /api/v1/clients?status={status}
  */
 app.get('/api/v1/clients', (req, res) => {
   const status = req.query.status;
   if (status) {
-    // status can only be either 'backlog' | 'in-progress' | 'complete'
     if (status !== 'backlog' && status !== 'in-progress' && status !== 'complete') {
       return res.status(400).send({
         'message': 'Invalid status provided.',
         'long_message': 'Status can only be one of the following: [backlog | in-progress | complete].',
       });
     }
-    const clients = db.prepare('select * from clients where status = ?').all(status);
+    const clients = db.prepare('select * from clients where status = ? order by priority').all(status);
     return res.status(200).send(clients);
   }
-  const statement = db.prepare('select * from clients');
+  const statement = db.prepare('select * from clients order by priority');
   const clients = statement.all();
   return res.status(200).send(clients);
 });
 
 /**
  * Get a client based on the id provided.
- * GET /api/v1/clients/{client_id} - get client by id
+ * GET /api/v1/clients/{client_id}
  */
 app.get('/api/v1/clients/:id', (req, res) => {
   const id = parseInt(req.params.id , 10);
   const { valid, messageObj } = validateId(id);
   if (!valid) {
-    res.status(400).send(messageObj);
+    return res.status(400).send(messageObj);
   }
   return res.status(200).send(db.prepare('select * from clients where id = ?').get(id));
 });
 
-/**
- * Update client information based on the parameters provided.
- * When status is provided, the client status will be changed
- * When priority is provided, the client priority will be changed with the rest of the clients accordingly
- * Note that priority = 1 means it has the highest priority (should be on top of the swimlane).
- * No client on the same status should not have the same priority.
- * This API should return list of clients on success
- *
- * PUT /api/v1/clients/{client_id} - change the status of a client
- *    Data:
- *      status (optional): 'backlog' | 'in-progress' | 'complete',
- *      priority (optional): integer,
- *
- */
+
 app.put('/api/v1/clients/:id', (req, res) => {
   const id = parseInt(req.params.id , 10);
   const { valid, messageObj } = validateId(id);
   if (!valid) {
-    res.status(400).send(messageObj);
+    return res.status(400).send(messageObj);
   }
 
   let { status, priority } = req.body;
-  let clients = db.prepare('select * from clients').all();
-  const client = clients.find(client => client.id === id);
+  const client = db.prepare('select * from clients where id = ?').get(id);
 
-  /* ---------- Update code below ----------*/
+  const targetStatus = status || client.status;
+  const targetPriority = priority || client.priority;
 
+  if (targetStatus === client.status) {
+    if (targetPriority < client.priority) {
+      db.prepare('UPDATE clients SET priority = priority + 1 WHERE status = ? AND priority >= ? AND priority < ? AND id != ?')
+        .run(targetStatus, targetPriority, client.priority, id);
+    } else if (targetPriority > client.priority) {
+     
+      db.prepare('UPDATE clients SET priority = priority - 1 WHERE status = ? AND priority <= ? AND priority > ? AND id != ?')
+        .run(targetStatus, targetPriority, client.priority, id);
+    }
+  } else {
+   
+    db.prepare('UPDATE clients SET priority = priority - 1 WHERE status = ? AND priority > ?')
+      .run(client.status, client.priority);
 
+    
+    db.prepare('UPDATE clients SET priority = priority + 1 WHERE status = ? AND priority >= ?')
+      .run(targetStatus, targetPriority);
+  }
 
-  return res.status(200).send(clients);
+  db.prepare('UPDATE clients SET status = ?, priority = ? WHERE id = ?')
+    .run(targetStatus, targetPriority, id);
+
+  const allClients = db.prepare('select * from clients order by priority').all();
+  return res.status(200).send(allClients);
 });
 
 app.listen(3001);
